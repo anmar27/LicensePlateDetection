98812b5f306809616fb99d59731888ea1152052e
Ammar <antonio.m.f2706@gmail.com>
2023-12-10 20:53:48 +0100

    Change in adaboost method and adding other scripts

--- trainLicense.m
+++ trainLicense.m
@@ -182,8 +182,7 @@ models = cell(1, numberOfStages);
 
 for stage = 1:numberOfStages
     fprintf('Training stage %d\n', stage);
-    
-    [model, falsePositives] = trainCascadeStage(datafeaturesTest,dataclassTest,datafeatures, dataclass, itt);
+    [model, falsePositives] = adabost(datafeaturesTest,dataclassTest,datafeatures, dataclass, itt);
     models{stage} = model;
     disp(size(falsePositives))
     
@@ -201,14 +200,13 @@ filename = sprintf('model_itt_%d_nOfStages_%d_size_%d.mat', itt, numberOfStages,
 % Save the datafeatures and dataclass variables to the dynamically named .mat file
 save(filename, "models");
 
-%% 
 
-% Calculate the training accuracy
-%accuracy = sum(dataclass == estimateclasstotal) / length(dataclass);
-%fprintf('Training Accuracy: %.2f%%\n', accuracy * 100);
 
 %% 
 clc;
+load("model_itt_25_nOfStages_4_size_860.mat");
+load("dataFeaturesClass_Pos400_Neg450.mat","datafeatures","dataclass","haarFeatureMapping");
+
 %Sliding window approach to find 
 inputImagePath = "C:\Users\Usuario\OneDrive - Hanzehogeschool Groningen\Escritorio\Matlab Uni\CV-Project\Project CV\001\CroppedVehicles\00430.jpg_vehicle_2.jpg";
 if isfile(inputImagePath)
@@ -217,46 +215,67 @@ if isfile(inputImagePath)
 end
 
 matches = [];
-originalImage = im2gray(originalImage);
-scales = [1];%, 1.25, 1.4,1.55,1.7]; 
+originalImage = im2gray(originalImage); 
 baseWindowSize = [17, 47]; 
 baseStepSize = 5; % Base step size for scale 1
+scales = 1; %Only using scale 1
+pyramidItt = 8; %Number of pyramid stages
 
-for scale = scales
-    % Rescale image
-    inputImage = imresize(originalImage, scale);
+model = cell(7, 1); % Create a cell array with 7 rows and 1 column
 
-    % Adjust window size and step size based on scale
-    windowSize = round(baseWindowSize * scale);
-    stepSize = round(baseStepSize * scale);
+% Assign each stage to a cell in the model array
+model{1} = selectedClassifiers(1:2,:);
+model{2} = selectedClassifiers(3:9,:);
+model{3} = selectedClassifiers(10:30,:);
+model{4} = selectedClassifiers(31:74,:);   
+model{5} = selectedClassifiers(75:124,:);
+model{6} = selectedClassifiers(125:199,:);
+model{7} = selectedClassifiers(200:300,:);
 
-    maxX = size(inputImage, 2) - windowSize(2) + 1;
-    maxY = size(inputImage, 1) - windowSize(1) + 1;
 
-    for x = 1:stepSize:maxX
-        for y = 1:stepSize:maxY
-            window = inputImage(y:y+windowSize(1)-1, x:x+windowSize(2)-1);
-            normalizedWindow = imresize(window, baseWindowSize);
-           
-            isPositive = true;
-            for stage = 1:numberOfStages
-                %classificationScore = adaboost('apply', windowFeatures, models{stage});
-                classificationScore = applyAdaboostModelToImage(models{stage}, normalizedWindow ,haarFeatureMapping);
-                if classificationScore ~= 1
-                    isPositive = false;
-                    break; % Window rejected by this stage
+for itr = 1:pyramidItt
+    printout = strcat('Iteration #',int2str(itr),'\n');
+    fprintf(printout);
+    for scale = scales
+        % Rescale image
+        inputImage = imresize(originalImage, scale);
+        
+        % Adjust window size and step size based on scale
+        windowSize = round(baseWindowSize * scale);
+        stepSize = round(baseStepSize * scale);
+    
+        maxX = size(inputImage, 2) - windowSize(2) + 1;
+        maxY = size(inputImage, 1) - windowSize(1) + 1;
+    
+        for x = 1:stepSize:maxX
+            for y = 1:stepSize:maxY
+                window = inputImage(y:y+windowSize(1)-1, x:x+windowSize(2)-1);
+                normalizedWindow = imresize(window, baseWindowSize);
+                normalizedWindow = integralImage(normalizedWindow);
+               
+                isPositive = true;
+                for stage = 1:numberOfStages
+                    classificationScore = cascadeClassifier(model{stage}, normalizedWindow ,haarFeatureMapping);
+                    if classificationScore ~= 1
+                        isPositive = false;
+                        break; % Window rejected by this stage
+                    end
                 end
-            end
-
-            if isPositive
-                % Window passed all stages, mark as positive detection
-                fprintf("Possible match in steps x:%d & y:%d",x,y);
-                matches = [matches; x, y, classificationScore,scale];
+    
+                if isPositive
+                    % Window passed all stages, mark as positive detection
+                    fprintf("Possible match in steps x:%d & y:%d in pyramid itt %d",x,y,itr);
+                    matches = [matches; x, y, classificationScore,scale];
+                end
             end
         end
     end
+    % create next image pyramid level
+    tempImg = imresize(inputImage,.8);
+    img = tempImg;
+    %new input image
+    inputImage = integralImg(img);
 end
-
 
 %% 
 
@@ -287,19 +306,6 @@ end
 hold off; % Release the hold on the figure
 
 
-%% 
-
-function [model, falsePositives] = trainCascadeStage(datafeaturesTest,dataclassTest,datafeatures, dataclass, itt)
-    
-    [~, model] = adaboost('train', datafeatures, dataclass, itt);
-
-    estimateclass = adaboost('apply', datafeaturesTest, model);
-    disp(size(estimateclass));
-    estimateclass = estimateclass';
-
-    % Identify false positives: Negative examples incorrectly classified as positive
-    falsePositives = datafeatures(estimateclass == 1 & dataclassTest == -1, :);
-end  
 
 %% 
 %%%Code that obtains the haarFeatures of one image
@@ -367,13 +373,13 @@ function intensity = getCorners(img,startX,startY,endX,endY)
     intensity = d-(b+c)+a; % by property of the integral image
 end
 
-function estimateclass = applyAdaboostModelToImage(model, inputImage, haarFeatureMapping)
+function estimateclass = cascadeClassifier(model, inputImage, haarFeatureMapping)
     % Preprocess the input image (this should match your training preprocessing)
     % For example, if you used integral images:
     % processedImage = integralImage(inputImage);
     inputImage = integralImg(inputImage);
 
-
+    
     % Initialize the sum of weak classifier results
     estimateclasssum = 0;
 
@@ -409,7 +415,7 @@ function estimateclass = applyAdaboostModelToImage(model, inputImage, haarFeatur
 end
 
 
-function haarValue = calcHaarValues(integralImage, haarType, pixelX, pixelY, dimX, dimY)
+function haarValue = calcHaarValues(img, haarType, pixelX, pixelY, dimX, dimY)
     % integralImage: The integral image
     % haarType: Type of Haar feature (determined by its dimensions)
     % pixelX, pixelY: Top left coordinates of the Haar feature
@@ -419,35 +425,35 @@ function haarValue = calcHaarValues(integralImage, haarType, pixelX, pixelY, dim
     haarValue = 0;
 
     function sum = rectSum(x1, y1, x2, y2)
-        sum = integralImage(y2, x2) - integralImage(y1, x2) - integralImage(y2, x1) + integralImage(y1, x1);
+        sum = img(y2, x2) - img(y1, x2) - img(y2, x1) + img(y1, x1);
     end
+
 
      switch haarType
         case 1 % Horizontal two-rectangle feature
+            midX = pixelX + round(dimX/2);
+            leftSum = rectSum(pixelX, pixelY, midX, pixelY + dimY);
+            rightSum = rectSum(midX, pixelY, pixelX + dimX, pixelY + dimY);
+            haarValue = leftSum - rightSum;  
+        case 2 % Vertical two-rectangle feature
             midY = pixelY + round(dimY/2);
             topSum = rectSum(pixelX, pixelY, pixelX + dimX, midY);
             bottomSum = rectSum(pixelX, midY, pixelX + dimX, pixelY + dimY);
             haarValue = topSum - bottomSum;
 
-        case 2 % Vertical two-rectangle feature
-            midX = pixelX + round(dimX/2);
-            leftSum = rectSum(pixelX, pixelY, midX, pixelY + dimY);
-            rightSum = rectSum(midX, pixelY, pixelX + dimX, pixelY + dimY);
-            haarValue = leftSum - rightSum;
-
         case 3 % Horizontal three-rectangle feature
-            thirdY = round(dimY / 3);
-            topSum = rectSum(pixelX, pixelY, pixelX + dimX, pixelY + thirdY);
-            middleSum = rectSum(pixelX, pixelY + thirdY, pixelX + dimX, pixelY + 2*thirdY);
-            bottomSum = rectSum(pixelX, pixelY + 2*thirdY, pixelX + dimX, pixelY + dimY);
-            haarValue = topSum - 2*middleSum + bottomSum;
-
-        case 4 % Vertical three-rectangle feature
             thirdX = round(dimX / 3);
             leftSum = rectSum(pixelX, pixelY, pixelX + thirdX, pixelY + dimY);
             middleSum = rectSum(pixelX + thirdX, pixelY, pixelX + 2*thirdX, pixelY + dimY);
             rightSum = rectSum(pixelX + 2*thirdX, pixelY, pixelX + dimX, pixelY + dimY);
-            haarValue = leftSum - 2*middleSum + rightSum;
+            haarValue = leftSum - middleSum + rightSum;
+
+        case 4 % Vertical three-rectangle feature
+            thirdY = round(dimY / 3);
+            topSum = rectSum(pixelX, pixelY, pixelX + dimX, pixelY + thirdY);
+            middleSum = rectSum(pixelX, pixelY + thirdY, pixelX + dimX, pixelY + 2*thirdY);
+            bottomSum = rectSum(pixelX, pixelY + 2*thirdY, pixelX + dimX, pixelY + dimY);
+            haarValue = topSum - middleSum + bottomSum;
 
         case 5 % Four-rectangle feature
             midX = pixelX + round(dimX/2);
@@ -460,142 +466,50 @@ function haarValue = calcHaarValues(integralImage, haarType, pixelX, pixelY, dim
     end
 end
 
-function [estimateclasstotal,model]=adaboost(mode,datafeatures,dataclass_or_model,itt)
-% This function AdaBoost, consist of two parts a simpel weak classifier and
-% a boostitg part:
-% The weak classifier tries to find the best treshold in one of the data
-% dimensions to sepparate the data into two classes -1 and 1
-% The boosting part calls the clasifier iteratively, after every classification
-% step it changes the weights of miss-classified examples. This creates a
-% cascade of "weak classifiers" which behaves like a "strong classifier"
-%
-%  Training mode:
-%    [estimateclass,model]=adaboost('train',datafeatures,dataclass,itt)
-%  Apply mode:
-%    estimateclass=adaboost('apply',datafeatures,model)
-%  inputs/outputs:
-%    datafeatures : An Array with size number_samples x number_features
-%    dataclass : An array with the class off all examples, the class
-%                 can be -1 or 1
-%    itt : The number of training itterations
-%    model : A struct with the cascade of weak-classifiers
-%    estimateclass : The by the adaboost model classified data
-switch(mode)
-    case 'train'
-        % Train the adaboost model
-        
-        % Set the data class 
-        dataclass=dataclass_or_model(:);
-        model=struct;
-        
-        % Weight of training samples, first every sample is even important
-        % (same weight)
-        D=ones(length(dataclass),1)/length(dataclass);
-        
-        % This variable will contain the results of the single weak
-        % classifiers weight by their alpha
-        estimateclasssum=zeros(size(dataclass));
-        
-        % Calculate max min of the data
-        boundary=[min(datafeatures,[],1) max(datafeatures,[],1)];
-        % Do all model training itterations
-        for t=1:itt
-            % Find the best treshold to separate the data in two classes
-            [estimateclass,err,h] = WeightedThresholdClassifier(datafeatures,dataclass,D);
-            % Weak classifier influence on total result is based on the current
-            % classification error
-            alpha=1/2 * log((1-err)/max(err,eps));
-            
-            % Store the model parameters
-            model(t).alpha = alpha;
-            model(t).dimension=h.dimension;
-            model(t).threshold=h.threshold;
-            model(t).direction=h.direction;
-            model(t).boundary = boundary;
-            % We update D so that wrongly classified samples will have more weight
-            D = D.* exp(-model(t).alpha.*dataclass.*estimateclass);
-            D = D./sum(D);
-            
-            % Calculate the current error of the cascade of weak
-            % classifiers
-            estimateclasssum=estimateclasssum +estimateclass*model(t).alpha;
-            estimateclasstotal=sign(estimateclasssum);
-            model(t).error=sum(estimateclasstotal~=dataclass)/length(dataclass);
-            if(model(t).error==0), break; end
+function [newWeights,alpha] = adaboost(classifier, images, imgWeights,positiveSize,imgsSize)
+captures = zeros(imgsSize,1);
+error = 0;
+
+for i = 1:imgsSize
+    img = images{i};
+    % obtains classifier metadata from fields in the row vector 
+    haar = classifier(1);
+    pixelX = classifier(2);
+    pixelY = classifier(3);
+    haarX = classifier(4);
+    haarY = classifier(5);
+    % calculates intensity difference between black-white region of the
+    % Haar feature and checks against the precalculated range
+    haarVal = calcHaarValues(img,haar,pixelX,pixelY,haarX,haarY);
+    if haarVal >= classifier(9) && haarVal <= classifier(10) % if falls between correct value
+        if i <= positiveSize % if its a license plate
+            captures(i) = 1; % correct capture
+        else
+            captures(i) = 0; % error
+            error = error + imgWeights(i); % increase weighted error count
         end
-        
-    case 'apply' 
-        % Apply Model on the test data
-        model=dataclass_or_model;
-        
-        % Limit datafeatures to orgininal boundaries
-        if(length(model)>1);
-            minb=model(1).boundary(1:end/2);
-            maxb=model(1).boundary(end/2+1:end);
-            datafeatures=bsxfun(@min,datafeatures,maxb);
-            datafeatures=bsxfun(@max,datafeatures,minb);
+    else % if falls outside the expected range
+        if i <= positiveSize % if is a Plate
+            captures(i) = 0;
+            error = error + imgWeights(i); % error
+        else 
+            captures(i) = 1;
         end
-    
-        % Add all results of the single weak classifiers weighted by their alpha 
-        estimateclasssum=zeros(size(datafeatures,1),1);
-        for t=1:length(model);
-            estimateclasssum=estimateclasssum+model(t).alpha*ApplyClassTreshold(model(t), datafeatures);
-        end
-        % If the total sum of all weak classifiers
-        % is less than zero it is probablly class -1 otherwise class 1;
-        estimateclasstotal=sign(estimateclasssum);
-        
-    otherwise
-        error('adaboost:inputs','unknown mode');
-end
-end
-function [estimateclass,err,h] = WeightedThresholdClassifier(datafeatures,dataclass,dataweight)
-% This is an example of an "Weak Classifier", it caculates the optimal
-% threshold for all data feature dimensions.
-% It then selects the dimension and  treshold which divides the 
-% data into two class with the smallest error.
-% Number of treshold steps
-ntre=2e3;
-% Split the data in two classes 1 and -1
-r1=datafeatures(dataclass<0,:); w1=dataweight(dataclass<0);
-r2=datafeatures(dataclass>0,:); w2=dataweight(dataclass>0);
-% Calculate the min and max for every dimensions
-minr=min(datafeatures,[],1)-1e-10; maxr=max(datafeatures,[],1)+1e-10;
-% Make a weighted histogram of the two classes
-p2c= ceil((bsxfun(@rdivide,bsxfun(@minus,r2,minr),(maxr-minr)))*(ntre-1)+1+1e-9);   p2c(p2c>ntre)=ntre;
-p1f=floor((bsxfun(@rdivide,bsxfun(@minus,r1,minr),(maxr-minr)))*(ntre-1)+1-1e-9);  p1f(p1f<1)=1;
-ndims=size(datafeatures,2);
-i1=repmat(1:ndims,size(p1f,1),1);  i2=repmat(1:ndims,size(p2c,1),1);
-h1f=accumarray([p1f(:) i1(:)],repmat(w1(:),ndims,1),[ntre ndims],[],0);
-h2c=accumarray([p2c(:) i2(:)],repmat(w2(:),ndims,1),[ntre ndims],[],0);
-% This function calculates the error for every all possible treshold value
-% and dimension
-h2ic=cumsum(h2c,1);
-h1rf=cumsum(h1f(end:-1:1,:),1); h1rf=h1rf(end:-1:1,:);
-e1a=h1rf+h2ic;
-e2a=sum(dataweight)-e1a;
-% We want the treshold value and dimension with the minimum error
-[err1a,ind1a]=min(e1a,[],1);  dim1a=(1:ndims); dir1a=ones(1,ndims);
-[err2a,ind2a]=min(e2a,[],1);  dim2a=(1:ndims); dir2a=-ones(1,ndims);
-A=[err1a(:),dim1a(:),dir1a(:),ind1a(:);err2a(:),dim2a(:),dir2a(:),ind2a(:)];
-[err,i]=min(A(:,1)); dim=A(i,2); dir=A(i,3); ind=A(i,4);
-thresholds = linspace(minr(dim),maxr(dim),ntre);
-thr=thresholds(ind);
-% Apply the new treshold
-h.dimension = dim; 
-h.threshold = thr; 
-h.direction = dir;
-estimateclass=ApplyClassTreshold(h,datafeatures);
-end
-function y = ApplyClassTreshold(h, x)
-% Draw a line in one dimension (like horizontal or vertical)
-% and classify everything below the line to one of the 2 classes
-% and everything above the line to the other class.
-if(h.direction == 1)
-    y =  double(x(:,h.dimension) >= h.threshold);
-else
-    y =  double(x(:,h.dimension) < h.threshold);
-end
-y(y==0) = -1;
+    end
 end
 
+alpha = 0.5*log((1-error)/error); % updates classifier weight (alpha)
+
+% modifies images' weights by whether it is a successful capture or not
+% correct captures result in lower weights; false captures result in higher
+% weight to put more emphasis on them
+for i = 1:imgsSize
+    if captures(i) == 0
+        imgWeights(i) = imgWeights(i).*exp(alpha);
+    else
+        imgWeights(i) = imgWeights(i).*exp(-alpha);
+    end
+end
+imgWeights = imgWeights./sum(imgWeights); % normalize image weights
+newWeights = imgWeights; % pass as function output
+end
